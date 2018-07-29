54f98e047707abccf1748e30b315ec02c351976e
Carl Philipp Reh <carlphilippreh@gmail.com>
2018-07-29 13:10:37 +0200

    Add -v option to compile_headers

--- compile_headers/compile_headers.cpp
+++ compile_headers/compile_headers.cpp
@@ -4,19 +4,42 @@
 #include <sge/parse/json/object.hpp>
 #include <sge/parse/json/parse_file_exn.hpp>
 #include <sge/parse/json/start.hpp>
+#include <fcppt/args_char.hpp>
+#include <fcppt/args_from_second.hpp>
 #include <fcppt/exception.hpp>
-#include <fcppt/extract_from_string.hpp>
-#include <fcppt/optional/to_exception.hpp>
+#include <fcppt/main.hpp>
+#include <fcppt/make_int_range_count.hpp>
+#include <fcppt/make_strong_typedef.hpp>
 #include <fcppt/string.hpp>
+#include <fcppt/strong_typedef_impl.hpp>
+#include <fcppt/strong_typedef_output.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/to_std_string.hpp>
 #include <fcppt/algorithm/contains.hpp>
+#include <fcppt/algorithm/map.hpp>
 #include <fcppt/config/platform.hpp>
+#include <fcppt/either/match.hpp>
+#include <fcppt/either/output.hpp>
 #include <fcppt/io/cerr.hpp>
 #include <fcppt/io/cout.hpp>
+#include <fcppt/optional/make.hpp>
+#include <fcppt/options/apply.hpp>
+#include <fcppt/options/error.hpp>
+#include <fcppt/options/make_default_value.hpp>
+#include <fcppt/options/long_name.hpp>
+#include <fcppt/options/option.hpp>
+#include <fcppt/options/optional_help_text.hpp>
+#include <fcppt/options/optional_short_name.hpp>
+#include <fcppt/options/parse.hpp>
+#include <fcppt/options/result_of.hpp>
+#include <fcppt/options/short_name.hpp>
+#include <fcppt/options/switch.hpp>
 #include <fcppt/preprocessor/disable_gcc_warning.hpp>
 #include <fcppt/preprocessor/pop_warning.hpp>
 #include <fcppt/preprocessor/push_warning.hpp>
+#include <fcppt/record/get.hpp>
+#include <fcppt/record/make_label.hpp>
+#include <fcppt/variant/output.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/join.hpp>
@@ -26,7 +49,6 @@
 #include <algorithm>
 #include <cstdlib>
 #include <exception>
-#include <functional>
 #include <iostream>
 #include <iterator>
 #include <string>
@@ -106,10 +128,16 @@ make_syntax_only(
 		);
 }
 
+FCPPT_MAKE_STRONG_TYPEDEF(
+	bool,
+	verbose
+);
+
 void
 worker(
 	sge::parse::json::value const &_element,
-	boost::asio::io_service &_io_service
+	boost::asio::io_service &_io_service,
+	verbose const _verbose
 )
 {
 	sge::parse::json::object const &json_object(
@@ -147,20 +175,33 @@ worker(
 			return;
 	}
 
-	int const exit_status(
-		std::system(
-			fcppt::to_std_string(
-				make_syntax_only(
-					sge::parse::json::find_member_exn<
-						sge::parse::json::string const
-					>(
-						json_object.members,
-						fcppt::string(
-							FCPPT_TEXT("command")
-						)
+	std::string const command{
+		fcppt::to_std_string(
+			make_syntax_only(
+				sge::parse::json::find_member_exn<
+					sge::parse::json::string const
+				>(
+					json_object.members,
+					fcppt::string(
+						FCPPT_TEXT("command")
 					)
 				)
-			).c_str()
+			)
+		)
+	};
+
+	if(
+		_verbose.get()
+	)
+		std::cout
+			<<
+			command
+			<<
+			'\n';
+
+	int const exit_status(
+		std::system(
+			command.c_str()
 		)
 	);
 
@@ -195,85 +236,24 @@ FCPPT_PP_POP_WARNING
 #endif
 }
 
-unsigned
-extract_threads(
-	int const _argc,
-	char const *const *const _argv
+FCPPT_MAKE_STRONG_TYPEDEF(
+	unsigned,
+	num_threads
+);
+
+void
+main_program(
+	num_threads const _num_threads,
+	verbose const _verbose
 )
 {
-	if(
-		_argc != 2
-	)
-		return
-			std::max(
-				std::thread::hardware_concurrency(),
-				1u
-			);
-
-	std::string const arg(
-		_argv[1]
-	);
-
-	if(
-		arg.size() < 2u
-		||
-		arg.substr(
-			0u,
-			2u
-		)
-		!=
-		"-j"
-	)
-		throw
-			fcppt::exception(
-				FCPPT_TEXT("Invalid parameter!")
-			);
-
-	return
-		fcppt::optional::to_exception(
-			fcppt::extract_from_string<
-				unsigned
-			>(
-				arg.substr(
-					2u
-				)
-			),
-			[]{
-				return
-					fcppt::exception{
-						FCPPT_TEXT("-j takes an int parameter")
-					};
-			}
-		);
-}
-
-}
-
-int
-main(
-	int argc,
-	char **argv
-)
-try
-{
-	if(
-		argc > 2
-	)
-	{
-		fcppt::io::cerr()
-			<< FCPPT_TEXT("This command takes no parameters or a -j parameter!\n");
-
-		return EXIT_FAILURE;
-	}
-
-	unsigned const num_threads(
-		extract_threads(
-			argc,
-			argv
-		)
-	);
-
-	fcppt::io::cout() << FCPPT_TEXT("Starting ") << num_threads << FCPPT_TEXT(" threads\n");
+	fcppt::io::cout()
+		<<
+		FCPPT_TEXT("Starting ")
+		<<
+		_num_threads
+		<<
+		FCPPT_TEXT(" threads\n");
 
 	sge::parse::json::array const build_commands(
 		sge::parse::json::parse_file_exn(
@@ -281,22 +261,26 @@ try
 		).array()
 	);
 
-	boost::asio::io_service io_service;
+	boost::asio::io_service io_service{};
 
 	for(
-		auto const &element : build_commands.elements
+		auto const &element
+		:
+		build_commands.elements
 	)
-	{
 		io_service.post(
-			std::bind(
-				worker,
+			[
+				&io_service,
 				element,
-				std::ref(
-					io_service
-				)
-			)
+				_verbose
+			]{
+				worker(
+					element,
+					io_service,
+					_verbose
+				);
+			}
 		);
-	}
 
 	typedef
 	std::vector<
@@ -304,48 +288,185 @@ try
 	>
 	thread_vector;
 
-	thread_vector threads;
+	thread_vector threads{
+		fcppt::algorithm::map<
+			thread_vector
+		>(
+			fcppt::make_int_range_count(
+				_num_threads.get()
+			),
+			[
+				&io_service
+			](
+				unsigned
+			){
+				return
+					std::thread{
+						[
+							&io_service
+						]()
+						{
+							io_service.run();
+						}
+					};
+			}
+		)
+	};
 
 	for(
-		unsigned index = 0;
-		index < num_threads;
-		++index
-	)
-		threads.push_back(
-			std::thread(
-				[
-					&io_service
-				]()
-				{
-					io_service.run();
-				}
-			)
-		);
-
-	for(
-		auto &thread : threads
+		auto &thread
+		:
+		threads
 	)
 		thread.join();
+}
 
-	return EXIT_SUCCESS;
+}
+
+FCPPT_PP_PUSH_WARNING
+FCPPT_PP_DISABLE_GCC_WARNING(-Wmissing-declarations)
+
+int
+FCPPT_MAIN(
+	int argc,
+	fcppt::args_char **argv
+)
+try
+{
+	FCPPT_RECORD_MAKE_LABEL(
+		verbose_label
+	);
+
+	FCPPT_RECORD_MAKE_LABEL(
+		num_threads_label
+	);
+
+	auto const parser(
+		fcppt::options::apply(
+			fcppt::options::switch_<
+				verbose_label
+			>{
+				fcppt::options::optional_short_name{
+					fcppt::options::short_name{
+						FCPPT_TEXT("v")
+					}
+				},
+				fcppt::options::long_name{
+					FCPPT_TEXT("verbose")
+				},
+				fcppt::options::optional_help_text{}
+			},
+			fcppt::options::option<
+				num_threads_label,
+				unsigned
+			>{
+				fcppt::options::optional_short_name{
+					fcppt::options::short_name{
+						FCPPT_TEXT("j")
+					}
+				},
+				fcppt::options::long_name{
+					FCPPT_TEXT("threads")
+				},
+				fcppt::options::make_default_value(
+					fcppt::optional::make(
+						std::max(
+							std::thread::hardware_concurrency(),
+							1u
+						)
+					)
+				),
+				fcppt::options::optional_help_text{}
+			}
+		)
+	);
+
+	return
+		fcppt::either::match(
+			fcppt::options::parse(
+				parser,
+				fcppt::args_from_second(
+					argc,
+					argv
+				)
+			),
+			[
+				&parser
+			](
+				fcppt::options::error const &_error
+			)
+			{
+				fcppt::io::cerr()
+					<<
+					_error
+					<<
+					FCPPT_TEXT('\n')
+					<<
+					parser.usage()
+					<<
+					FCPPT_TEXT('\n');
+
+				return
+					EXIT_FAILURE;
+			},
+			[](
+				fcppt::options::result_of<
+					decltype(
+						parser
+					)
+				> const &_args
+			)
+			{
+				main_program(
+					num_threads{
+						fcppt::record::get<
+							num_threads_label
+						>(
+							_args
+						)
+					},
+					verbose{
+						fcppt::record::get<
+							verbose_label
+						>(
+							_args
+						)
+					}
+				);
+
+				return
+					EXIT_SUCCESS;
+			}
+		);
+
+	return
+		EXIT_SUCCESS;
 }
 catch(
 	fcppt::exception const &_exception
 )
 {
 	fcppt::io::cerr()
-		<< _exception.string()
-		<< FCPPT_TEXT('\n');
+		<<
+		_exception.string()
+		<<
+		FCPPT_TEXT('\n');
 
-	return EXIT_FAILURE;
+	return
+		EXIT_FAILURE;
 }
 catch(
 	std::exception const &_exception
 )
 {
 	std::cerr
-		<< _exception.what()
-		<< '\n';
+		<<
+		_exception.what()
+		<<
+		'\n';
 
-	return EXIT_FAILURE;
+	return
+		EXIT_FAILURE;
 }
+
+FCPPT_PP_POP_WARNING

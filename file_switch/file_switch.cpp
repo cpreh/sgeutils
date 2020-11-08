8823854a2493b58b3cf1cdcd139b08cf8bd39b73
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-11-08 10:15:17 +0100

    Update file_switch

--- file_switch/file_switch.cpp
+++ file_switch/file_switch.cpp
@@ -1,20 +1,34 @@
+#include <fcppt/args_char.hpp>
+#include <fcppt/args_from_second.hpp>
+#include <fcppt/main.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/assert/error_message.hpp>
+#include <fcppt/either/match.hpp>
+#include <fcppt/options/apply.hpp>
+#include <fcppt/options/argument.hpp>
+#include <fcppt/options/error.hpp>
+#include <fcppt/options/error_output.hpp>
+#include <fcppt/options/long_name.hpp>
+#include <fcppt/options/optional_help_text.hpp>
+#include <fcppt/options/optional_short_name.hpp>
+#include <fcppt/options/parse.hpp>
+#include <fcppt/options/result_of.hpp>
+#include <fcppt/options/switch.hpp>
+#include <fcppt/record/get.hpp>
+#include <fcppt/record/make_label.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <boost/next_prior.hpp>
-#include <algorithm>
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <numeric>
 #include <ostream>
 #include <string>
-#include <vector>
 #include <fcppt/config/external_end.hpp>
 
+namespace
+{
 /*
   Example:
 
@@ -54,46 +68,28 @@
  */
 
 int
-main(
-	int argc,
-	char **argv
+main_program(
+	std::filesystem::path const &_input_file_with_extension,
+	bool const _debug_mode
 )
-try
 {
-	if(
-		argc <= 1)
+	std::filesystem::path input_directory{
+		_input_file_with_extension.parent_path()
+	};
+
+	std::filesystem::path namespace_path{};
+
+	if(_debug_mode)
 	{
-		std::cerr
-			<< "Usage: "
-			<< argv[0]
-			<< " <hpp-or-cpp>\n";
-
-		return EXIT_FAILURE;
-	}
-
-	std::filesystem::path const
-		input_file_with_extension(
-			argv[1]);
-
-	std::filesystem::path
-		input_directory(
-			input_file_with_extension.parent_path()),
-		namespace_path;
-
-	bool const debug_mode =
-		false;
-
-	if(debug_mode)
-	{
-		std::cout << "input_file_with_extension: " << input_file_with_extension << "\n";
+		std::cout << "input_file_with_extension: " << _input_file_with_extension << "\n";
 		std::cout << "input_directory: " << input_directory << "\n";
 	}
 
 	while(
-		!std::filesystem::is_directory(input_directory / "src") ||
-		!std::filesystem::is_directory(input_directory / "include"))
+		!std::filesystem::is_directory(input_directory / "src") || // NOLINT(fuchsia-default-arguments-calls)
+		!std::filesystem::is_directory(input_directory / "include")) // NOLINT(fuchsia-default-arguments-calls)
 	{
-		namespace_path = *(boost::prior(input_directory.end())) / namespace_path;
+		namespace_path = *(std::prev(input_directory.end())) / namespace_path; // NOLINT(fuchsia-default-arguments-calls)
 		input_directory = input_directory.parent_path();
 
 		if(input_directory.empty())
@@ -102,7 +98,7 @@ try
 			return EXIT_FAILURE;
 		}
 
-		if(debug_mode)
+		if(_debug_mode)
 		{
 			std::cout << "input_directory: " << input_directory << "\n";
 			std::cout << "namespace path: " << namespace_path << "\n";
@@ -115,16 +111,16 @@ try
 		FCPPT_TEXT("Not sure what to do here..."));
 
 	// I'm not sure if this is a good test.
-	bool const is_header = namespace_path.begin()->string<char>() == "include";
+	bool const is_header = namespace_path.begin()->string<char>() == "include"; // NOLINT(fuchsia-default-arguments-calls)
 
-	if(is_header && input_file_with_extension.extension().string<char>() != ".hpp")
+	if(is_header && _input_file_with_extension.extension().string<char>() != ".hpp") // NOLINT(fuchsia-default-arguments-calls)
 	{
-		std::cerr << "Got a file in include/ which doesn't end in \"hpp\" (extension is " << input_file_with_extension.extension().string<char>() << ").\n";
+		std::cerr << "Got a file in include/ which doesn't end in \"hpp\" (extension is " << _input_file_with_extension.extension().string<char>() << ").\n"; // NOLINT(fuchsia-default-arguments-calls)
 		std::cerr << "Not sure what to do, exiting...\n";
 		return EXIT_FAILURE;
 	}
 
-	if(!is_header && input_file_with_extension.extension().string<char>() != ".cpp")
+	if(!is_header && _input_file_with_extension.extension().string<char>() != ".cpp") // NOLINT(fuchsia-default-arguments-calls)
 	{
 		std::cerr << "Got a file in src/ which doesn't end in \"cpp\". Not sure what to do, exiting...\n";
 		return EXIT_FAILURE;
@@ -136,7 +132,7 @@ try
 
 	std::filesystem::path const trimmed_namespace_path =
 		std::accumulate(
-			boost::next(
+			std::next(
 				namespace_path.begin(),
 				is_header
 				?
@@ -145,40 +141,133 @@ try
 					1),
 			namespace_path.end(),
 			std::filesystem::path(),
-			std::divides<std::filesystem::path>());
+			std::divides<>());
 
-	if(debug_mode)
+	if(_debug_mode)
 	{
 		std::cout << "Trimmed namespace path: " << trimmed_namespace_path << "\n";
 	}
 
 	std::filesystem::directory_iterator include_directory_iterator(
-		input_directory / "include");
+		input_directory / "include"); // NOLINT(fuchsia-default-arguments-calls)
 
 	if(include_directory_iterator == std::filesystem::directory_iterator())
 	{
-		std::cerr << "The include directory " << (input_directory / "include") << " is empty. Exiting...\n";
+		std::cerr << "The include directory " << (input_directory / "include") << " is empty. Exiting...\n"; // NOLINT(fuchsia-default-arguments-calls)
 		return EXIT_FAILURE;
 	}
 
 	std::string const namespace_name(
-		include_directory_iterator->path().stem().string<char>());
+		include_directory_iterator->path().stem().string<char>()); // NOLINT(fuchsia-default-arguments-calls)
 
-	if(debug_mode)
+	if(_debug_mode)
 	{
 		std::cout << "Namespace name: " << namespace_name << "\n";
 	}
 
 	if(is_header)
 	{
-		std::cout << (input_directory / "src" / trimmed_namespace_path / input_file_with_extension.stem().replace_extension(".cpp")).string<char>();
+		std::cout << (input_directory / "src" / trimmed_namespace_path / _input_file_with_extension.stem().replace_extension(".cpp")).string<char>(); // NOLINT(fuchsia-default-arguments-calls)
 	}
 	else
 	{
-		std::cout << (input_directory / "include" / namespace_name / trimmed_namespace_path / input_file_with_extension.stem().replace_extension(".hpp")).string<char>();
+		std::cout << (input_directory / "include" / namespace_name / trimmed_namespace_path / _input_file_with_extension.stem().replace_extension(".hpp")).string<char>(); // NOLINT(fuchsia-default-arguments-calls)
 	}
 
 	return EXIT_SUCCESS;
+}
+
+}
+
+int
+FCPPT_MAIN(
+	int argc,
+	fcppt::args_char **argv
+)
+try
+{
+	FCPPT_RECORD_MAKE_LABEL(
+		file_label
+	);
+
+	FCPPT_RECORD_MAKE_LABEL(
+		debug_label
+	);
+
+	auto const parser{
+		fcppt::options::apply(
+			fcppt::options::argument<
+				file_label,
+				std::filesystem::path
+			>{
+				fcppt::options::long_name{
+					FCPPT_TEXT("file")
+				},
+				fcppt::options::optional_help_text{}
+			},
+			fcppt::options::switch_<
+				debug_label
+			>{
+				fcppt::options::optional_short_name{},
+				fcppt::options::long_name{
+					FCPPT_TEXT("debug")
+				},
+				fcppt::options::optional_help_text{}
+			}
+		)
+	};
+
+	return
+		fcppt::either::match(
+			fcppt::options::parse(
+				parser,
+				fcppt::args_from_second(
+					argc,
+					argv
+				)
+			),
+			[
+				&parser
+			](
+				fcppt::options::error const &_error
+			)
+			{
+				fcppt::io::cerr()
+					<<
+					_error
+					<<
+					FCPPT_TEXT("\nUsage:\n")
+					<<
+					parser.usage()
+					<<
+					FCPPT_TEXT('\n');
+
+				return
+					EXIT_FAILURE;
+			},
+			[](
+				fcppt::options::result_of<
+					decltype(
+						parser
+					)
+				> const &_result
+			)
+			{
+				return
+					main_program(
+						fcppt::record::get<
+							file_label
+						>(
+							_result
+						),
+						fcppt::record::get<
+							debug_label
+						>(
+							_result
+						)
+					);
+			}
+		);
 }
 catch(
 	std::exception const &_error
